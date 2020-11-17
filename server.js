bd08ce180870964b650d57bee37a2d3d88773136
tahnoun <ts2010@hw.ac.uk>
2020-11-17 18:42:29 +0400

    server.js made

---
+++ server.js
@@ -0,0 +1,24 @@
+//server side
+
+//create server
+const express = require('express');
+const app = express();
+
+//allowing static file send from folder "views"
+app.use(express.static('views'));
+ 
+//sendin ghte index html file
+app.get('/', function(request, response) {
+    response.sendFile('Index.html', { root: __dirname });
+});
+
+//setting up port
+const port = process.env.PORT || 3000;
+const server = app.listen(port, function() {
+    console.log(`Our game listening on port : ${port}`);
+});
+
+//loading socket.io and binding to the server
+const io = require('socket.io')(server);
+
+ 
